c6763ba87db2ad9221f1d4655cced49056384d24
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-29 22:21:23 +0530

    added a input

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ const page = () => {
       <h1 className="bg-black text-white p-5 text-5xl text-center font-bold ">
         The To Do List
       </h1>
+      <form>
+        <input type="text" placeholder="Add..." />
+      </form>
     </>
   );
 };
