6658bd8d4f25e6973a07c0f054f5d64ca60bb74d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-29 22:19:57 +0530

    added h1 and styled

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 const page = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1 className="bg-black text-white p-5 text-5xl text-center font-bold ">
+        The To Do List
+      </h1>
+    </>
+  );
 };
 
 export default page;
