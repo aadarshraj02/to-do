d40298dcf8d2f7c1c2d79ba6b49b6693debc9510
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-29 22:24:10 +0530

    styled input

--- app/page.js
+++ app/page.js
@@ -5,7 +5,11 @@ const page = () => {
         The To Do List
       </h1>
       <form>
-        <input type="text" placeholder="Add..." />
+        <input
+          type="text"
+          placeholder="Enter Tasks Here..."
+          className="text-2xl border-2 border-zinc-800 m-8 px-4 py-2"
+        />
       </form>
     </>
   );
